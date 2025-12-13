1bb6524870604deff0ef1d7fafb98ce5dbe95a83
q965786x <kasatka74@yandex.ru>
2025-12-13 18:23:52 +0700

    Верстка таблицы и формы на главной  странице

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {} from "./Calendar.styled";
+
+{/* Календарь июль 2024 */}
+<div className="calendar-month">
+    <div className="month-header">Июль 2024</div>
+        <div className="weekdays">
+            <div className="weekday">пн</div>
+            <div className="weekday">вт</div>
+            <div className="weekday">ср</div>
+            <div className="weekday">чт</div>
+            <div className="weekday">пт</div>
+            <div className="weekday">сб</div>
+            <div className="weekday">вс</div>
+        </div>
+
+        {/* Пустые ячейки для выравнивания (июль 2024 начинается с понедельника) */}
+        <div className="days">
+            <div className="day">1</div>
+            <div className="day">2</div>
+            <div className="day">3</div>
+            <div className="day">4</div>
+            <div className="day">5</div>
+            <div className="day">6</div>
+            <div className="day">7</div>
+            <div className="day">8</div>
+            <div className="day">9</div>
+            <div className="day active">10</div>
+            <div className="day">11</div>
+            <div className="day">12</div>
+            <div className="day">13</div>
+            <div className="day">14</div>
+            <div className="day">15</div>
+            <div className="day">16</div>
+            <div className="day">17</div>
+            <div className="day">18</div>
+            <div className="day">19</div>
+            <div className="day">20</div>
+            <div className="day">21</div>
+            <div className="day">22</div>
+            <div className="day">23</div>
+            <div className="day">24</div>
+            <div className="day">25</div>
+            <div className="day">26</div>
+            <div className="day">27</div>
+            <div className="day">28</div>
+            <div className="day">29</div>
+            <div className="day">30</div>
+            <div className="day">31</div>
+        </div>
+</div>
+
+{/* Календарь август 2024 */}
+<div className="calendar-month">
+    <div className="month-header">Август 2024</div>
+        <div className="weekdays">
+            <div className="weekday">пн</div>
+            <div className="weekday">вт</div>
+            <div className="weekday">ср</div>
+            <div className="weekday">чт</div>
+            <div className="weekday">пт</div>
+            <div className="weekday">сб</div>
+            <div className="weekday">вс</div>
+        </div>
+
+        <div className="days">
+            {/* Пустые ячейки (август 2024 начинается с четверга) */}
+            <div className="day empty"></div>
+            <div className="day empty"></div>
+            <div className="day empty"></div>
+            <div className="day">1</div>
+            <div className="day">2</div>
+            <div className="day">3</div>
+            <div className="day">4</div>
+            <div className="day">5</div>
+            <div className="day">6</div>
+            <div className="day">7</div>
+            <div className="day">8</div>
+            <div className="day">9</div>
+            <div className="day">10</div>
+            <div className="day">11</div>
+            <div className="day">12</div>
+            <div className="day">13</div>
+            <div className="day">14</div>
+            <div className="day">15</div>
+            <div className="day">16</div>
+            <div className="day">17</div>
+            <div className="day">18</div>
+            <div className="day">19</div>
+            <div className="day">20</div>
+            <div className="day">21</div>
+            <div className="day">22</div>
+            <div className="day">23</div>
+            <div className="day">24</div>
+            <div className="day">25</div>
+            <div className="day">26</div>
+            <div className="day">27</div>
+            <div className="day">28</div>
+            <div className="day">29</div>
+            <div className="day">30</div>
+            <div className="day">31</div>
+        </div>            
+    
+</div> 
